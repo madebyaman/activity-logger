8f083fbe457a628da6f60fa787201074c87ee12c
Aman Thakur <builtbyaman@gmail.com>
2022-02-01 17:35:30 +0530

    Added signin to api

---
+++ pages/api/signin.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
+import prisma from '../../utils/prisma';
+
+export default async function signup(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = jwt.sign(
+      {
+        email: user?.email,
+        time: Date.now(),
+        id: user.id,
+      },
+      'hello',
+      { expiresIn: '8h' }
+    );
+
+    res.setHeader(
+      'Set-Cookie',
+      cookie.serialize('ACTIVITY_LOGGER_TOKEN', token, {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        path: '/',
+        sameSite: 'lax',
+        secure: process.env.NODE_ENV === 'production',
+      })
+    );
+
+    return res.json(user);
+  }
+  res.status(401);
+  res.json({ error: 'User already exists' });
+  return;
+}
