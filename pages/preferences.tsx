cdf017785a2e2efbded9b8cac87157463c1b8e8b
Aman Thakur <builtbyaman@gmail.com>
2022-02-21 23:31:21 +0530

    user preferences design wise ✅

--- pages/preferences.tsx
+++ pages/preferences.tsx
@@ -190,7 +190,7 @@ const Preferences: NextPageWithAuth = () => {
                 Push Notifications
               </h3>
               <p className="mt-1 text-sm text-gray-600">
-                Decide which communications you'd like to receive and how.
+                Decide whether you would like to receive push notifications
               </p>
             </div>
           </div>
@@ -199,130 +199,48 @@ const Preferences: NextPageWithAuth = () => {
               <div className="shadow overflow-hidden sm:rounded-md">
                 <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
                   <fieldset>
-                    <legend className="text-base font-medium text-gray-900">
-                      By Email
-                    </legend>
                     <div className="mt-4 space-y-4">
                       <div className="flex items-start">
                         <div className="flex items-center h-5">
                           <input
-                            id="comments"
-                            name="comments"
+                            id="push-notifications"
+                            name="push-notifications-state"
                             type="checkbox"
                             className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
                           />
                         </div>
                         <div className="ml-3 text-sm">
                           <label
-                            htmlFor="comments"
+                            htmlFor="push-notifications"
                             className="font-medium text-gray-700"
                           >
-                            Comments
+                            Push Notifications
                           </label>
                           <p className="text-gray-500">
-                            Get notified when someones posts a comment on a
-                            posting.
-                          </p>
-                        </div>
-                      </div>
-                      <div className="flex items-start">
-                        <div className="flex items-center h-5">
-                          <input
-                            id="candidates"
-                            name="candidates"
-                            type="checkbox"
-                            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                          />
-                        </div>
-                        <div className="ml-3 text-sm">
-                          <label
-                            htmlFor="candidates"
-                            className="font-medium text-gray-700"
-                          >
-                            Candidates
-                          </label>
-                          <p className="text-gray-500">
-                            Get notified when a candidate applies for a job.
-                          </p>
-                        </div>
-                      </div>
-                      <div className="flex items-start">
-                        <div className="flex items-center h-5">
-                          <input
-                            id="offers"
-                            name="offers"
-                            type="checkbox"
-                            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                          />
-                        </div>
-                        <div className="ml-3 text-sm">
-                          <label
-                            htmlFor="offers"
-                            className="font-medium text-gray-700"
-                          >
-                            Offers
-                          </label>
-                          <p className="text-gray-500">
-                            Get notified when a candidate accepts or rejects an
-                            offer.
+                            Do you want to be notified via push notifications?
                           </p>
                         </div>
                       </div>
                     </div>
                   </fieldset>
+
                   <fieldset>
-                    <div>
-                      <legend className="text-base font-medium text-gray-900">
-                        Push Notifications
-                      </legend>
-                      <p className="text-sm text-gray-500">
-                        These are delivered via SMS to your mobile phone.
-                      </p>
-                    </div>
-                    <div className="mt-4 space-y-4">
-                      <div className="flex items-center">
-                        <input
-                          id="push-everything"
-                          name="push-notifications"
-                          type="radio"
-                          className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                        />
-                        <label
-                          htmlFor="push-everything"
-                          className="ml-3 block text-sm font-medium text-gray-700"
-                        >
-                          Everything
-                        </label>
-                      </div>
-                      <div className="flex items-center">
-                        <input
-                          id="push-email"
-                          name="push-notifications"
-                          type="radio"
-                          className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                        />
-                        <label
-                          htmlFor="push-email"
-                          className="ml-3 block text-sm font-medium text-gray-700"
-                        >
-                          Same as email
-                        </label>
-                      </div>
-                      <div className="flex items-center">
-                        <input
-                          id="push-nothing"
-                          name="push-notifications"
-                          type="radio"
-                          className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                        />
-                        <label
-                          htmlFor="push-nothing"
-                          className="ml-3 block text-sm font-medium text-gray-700"
-                        >
-                          No push notifications
-                        </label>
-                      </div>
-                    </div>
+                    <label
+                      htmlFor="country"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Notify after every
+                    </label>
+                    <select
+                      id="country"
+                      name="country"
+                      autoComplete="country-name"
+                      className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    >
+                      <option>1 hour</option>
+                      <option>2 hours</option>
+                      <option>4 hours</option>
+                    </select>
                   </fieldset>
                 </div>
                 <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
