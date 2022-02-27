9c0347af9b4e4487cf983a40c0d7553d2e6c716a
Aman Thakur <builtbyaman@gmail.com>
2022-02-28 00:26:36 +0530

    added nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import type { AppProps } from 'next/app';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 
 import '../styles/globals.css';
+import '../styles/nprogress.css';
 import { NextPageWithAuth } from '../types';
 import {
   FlashMessageProvider,
   FlashMessageWrapper,
 } from '../components/FlashMessage';
 import { AppLayout } from '../components/ui/AppLayout';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 type ComponentPropsWithAuth = AppProps & {
   Component: NextPageWithAuth;
