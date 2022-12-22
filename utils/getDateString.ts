dcbef831f009a1b9e80bc65dbdde3d5102d19ea9
Aman Thakur <builtbyaman@gmail.com>
2022-12-23 01:48:02 +0530

    fixed formattedTime

--- utils/getDateString.ts
+++ utils/getDateString.ts
@@ -2,6 +2,6 @@ import { add, format } from 'date-fns';
 
 // Date in 12/16/2022
 export function dateString(offset: number) {
-  const date = add(new Date(), { minutes: 720 - offset });
+  const date = add(new Date(), { minutes: offset - 720 });
   return format(date, 'MM/dd/y');
 }
