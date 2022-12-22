0232fab42067615052317407689f1c89d117807a
Aman Thakur <builtbyaman@gmail.com>
2022-12-22 15:52:36 +0530

    fixing date

--- components/dashboard/showBlock.ts
+++ components/dashboard/showBlock.ts
@@ -1,10 +1,11 @@
-import { compareAsc } from 'date-fns';
-
 /**
  * Returns if a block should be allowed to edit.
  */
 export const showBlock = (to: Date): boolean => {
   const currentTime = new Date();
-  console.log(to);
-  return compareAsc(new Date(to), currentTime) === -1 ? true : false;
+  const currentTimeInUTC = currentTime.toUTCString();
+  const toTime = new Date(to);
+  const toTimeInUTC = toTime.toUTCString();
+  console.log(currentTimeInUTC, toTimeInUTC);
+  return new Date(currentTimeInUTC) > new Date(toTimeInUTC);
 };
