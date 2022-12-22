7c46d43a3e9a29265fc04299fded9e93ac6c58f1
Aman Thakur <builtbyaman@gmail.com>
2022-12-23 02:15:17 +0530

    fixing date issues

--- components/activity/ShowActivity.tsx
+++ components/activity/ShowActivity.tsx
@@ -67,9 +67,7 @@ export function ShowActivity({ activity }: { activity: Activity }) {
   }
 
   function formattedTime(from: string) {
-    const offset = new Date().getTimezoneOffset();
-    const parsedDate = parseISO(from);
-    const timeInCurrentZone = add(parsedDate, { minutes: offset - 720 }); // Negative b/c it needs to convert GMT to local time
+    const timeInCurrentZone = new Date(from);
     return format(timeInCurrentZone, 'h:mm a');
   }
 
--- utils/getDateString.ts
+++ utils/getDateString.ts
@@ -2,6 +2,6 @@ import { add, format } from 'date-fns';
 
 // Date in 12/16/2022
 export function dateString(offset: number) {
-  const date = add(new Date(), { minutes: offset - 720 });
+  const date = add(new Date(), { minutes: -offset });
   return format(date, 'MM/dd/y');
 }
