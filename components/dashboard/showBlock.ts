f014f22c54b6ccf12eed0fa09494dbc94cb2c1ab
Aman Thakur <builtbyaman@gmail.com>
2022-12-22 22:29:08 +0530

    date issues fixed ✅

--- components/dashboard/showBlock.ts
+++ components/dashboard/showBlock.ts
@@ -1,8 +1,14 @@
+import { zonedTimeToUtc } from 'date-fns-tz';
+
 /**
  * Returns if a block should be allowed to edit.
  */
 export const showBlock = (to: string | Date): boolean => {
-  const currentTime = new Date();
+  const currentTime = zonedTimeToUtc(new Date(), 'Europe/London');
   const toTime = new Date(to);
-  return currentTime.getTime() > toTime.getTime();
+  if (currentTime.getTime() > toTime.getTime()) {
+    return true;
+  } else {
+    return false;
+  }
 };
