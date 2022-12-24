06bb56cc19d8155d311b712c81432fd4a82909f3
Aman Thakur <builtbyaman@gmail.com>
2022-12-25 01:16:07 +0530

    date string issue fixed

--- utils/getDateString.ts
+++ utils/getDateString.ts
@@ -1,9 +1,9 @@
-import { add, format } from 'date-fns';
+import { sub, format } from 'date-fns';
 import { zonedTimeToUtc } from 'date-fns-tz';
 
 // Date in 12/16/2022
 export function dateString(offset: number) {
   const utcTime = zonedTimeToUtc(new Date(), 'Europe/London');
-  const date = add(utcTime, { minutes: offset });
+  const date = sub(utcTime, { minutes: offset });
   return format(date, 'MM/dd/y');
 }
