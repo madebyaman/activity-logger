3c95bf3e76d0614164ce553c43baf8ef388512e0
Aman Thakur <builtbyaman@gmail.com>
2023-01-02 20:46:54 +0530

    demo video

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,126 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Image from 'next/image';
 import { useProfile } from '@/utils';
 
 export default function HomeComponent() {
   const { profile } = useProfile();
 
   return (
-    <div className="bg-white mx-auto mt-4 mb-8 px-4">
-      <Head>
-        <title>Activity Logger App</title>
-      </Head>
-      <header className="max-w-7xl mx-auto py-6 px-4 sm:px-4 lg:px-8">
-        <nav>
-          <ul className="flex justify-between items-center">
-            <li className="uppercase text-sm font-semibold text-gray-700">
-              Activity Logger App
-            </li>
-            <li>
-              <Link
-                className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                href={profile ? '/dashboard' : '/signin'}
-              >
-                {profile ? 'Dashboard' : 'Login'}
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-        <div className="bg-blue-700 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
-          <div className="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
-            <div className="lg:self-center">
-              <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
-                <span className="block mb-2">Take control of your time</span>
-                <span className="block">
-                  Signup today and start using the app.
-                </span>
-              </h2>
-              <p className="mt-4 text-lg leading-6 text-indigo-200">
-                Activity Logger is an app to help you keep track of your time.
-                After every few hours come back to the app and enter what you
-                were doing in that time.
-              </p>
-              <Link
-                className="mt-8 bg-white border border-transparent rounded-md shadow px-5 py-3 inline-flex items-center text-base font-medium text-gray-600 hover:bg-gray-300"
-                href="/signin"
-              >
-                Start tracking your time
-              </Link>
+    <div className="isolate bg-white">
+      <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+        <svg
+          className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+          viewBox="0 0 1155 678"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+            fillOpacity=".3"
+            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+          />
+          <defs>
+            <linearGradient
+              id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+              x1="1155.49"
+              x2="-78.208"
+              y1=".177"
+              y2="474.645"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#9089FC" />
+              <stop offset={1} stopColor="#FF80B5" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+      <div className="mx-auto mt-4 mb-8 px-4">
+        <Head>
+          <title>Activity Logger App</title>
+        </Head>
+        <header className="max-w-7xl mx-auto py-6 px-4 sm:px-4 lg:px-8">
+          <nav>
+            <ul className="flex justify-between items-center">
+              <li className="uppercase text-sm font-semibold text-gray-700">
+                Activity Logger App
+              </li>
+              <li>
+                <Link
+                  className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                  href={profile ? '/dashboard' : '/signin'}
+                >
+                  {profile ? 'Dashboard' : 'Login'}
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <div className="relative px-6 lg:px-8">
+            <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+              <div>
+                <div>
+                  <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
+                    Take control of your time
+                  </h1>
+                  <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+                    Activity Logger is an app to help you keep track of your
+                    time. After every few hours come back to the app and enter
+                    what you were doing in that time.
+                  </p>
+                  <div className="mt-8 flex gap-x-4 sm:justify-center">
+                    <Link
+                      href="/signin"
+                      className="inline-block rounded-lg bg-blue-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-blue-600 hover:bg-blue-700 hover:ring-blue-700"
+                    >
+                      Get started
+                      <span className="ml-2 text-blue-200" aria-hidden="true">
+                        &rarr;
+                      </span>
+                    </Link>
+                    <Link
+                      href="https://www.loom.com/share/a95eec8cb13e4dcab1936973b9a09cd2"
+                      target={'_blank'}
+                      className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+                    >
+                      Watch Demo in Loom
+                      <span className="ml-2 text-gray-400" aria-hidden="true">
+                        &rarr;
+                      </span>
+                    </Link>
+                  </div>
+                </div>
+                <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+                  <svg
+                    className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+                    viewBox="0 0 1155 678"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+                      fillOpacity=".3"
+                      d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+                    />
+                    <defs>
+                      <linearGradient
+                        id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+                        x1="1155.49"
+                        x2="-78.208"
+                        y1=".177"
+                        y2="474.645"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop stopColor="#9089FC" />
+                        <stop offset={1} stopColor="#FF80B5" />
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="-mt-6 aspect-w-5 aspect-h-3 md:aspect-w-2 md:aspect-h-1">
-            <Image
-              className="transform translate-x-6 translate-y-6 rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20"
-              src="/screenshot.png"
-              alt="App screenshot"
-              priority
-              width={1000}
-              height={778}
-            />
-          </div>
-        </div>
+        </main>
       </div>
     </div>
   );
